e69972ae9efd9967985530c545f4464cbaabeb99
Alberto Rodriguez <arodu.dev@gmail.com>
2022-02-09 17:45:16 -0400

    cake4 support

--- src/Command/BookCommand.php
+++ src/Command/BookCommand.php
@@ -1,35 +1,29 @@
 <?php
+namespace CakeDC\Book\Command;
 
-/**
- * Copyright 2020 - 2022, Cake Development Corporation (https://www.cakedc.com)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright 2020 - 2022, Cake Development Corporation (https://www.cakedc.com)
- * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
-namespace CakeDC\Book\Shell;
-
-use Cake\Console\Shell;
+use Cake\Console\Arguments;
+use Cake\Console\Command;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
 use Cake\Core\Configure;
 use Cake\Http\Client;
 
 /**
- * Book shell command.
+ * Book command.
  */
-class BookShell extends Shell
+class BookCommand extends Command
 {
     /**
-     * Manage the available sub-commands along with their arguments and help
+     * Hook method for defining this command's option parser.
      *
-     * @see http://book.cakephp.org/3.0/en/console-and-shells.html#configuring-options-and-generating-help
-     * @return \Cake\Console\ConsoleOptionParser
+     * @see https://book.cakephp.org/3.0/en/console-and-shells/commands.html#defining-arguments-and-options
+     *
+     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
+     * @return \Cake\Console\ConsoleOptionParser The built parser.
      */
-    public function getOptionParser()
+    public function buildOptionParser(ConsoleOptionParser $parser)
     {
-        $parser = parent::getOptionParser();
+        $parser = parent::buildOptionParser($parser);
 
         $parser->addArgument('parameter', ['help' => 'Search parameter', 'required' => true]);
 
@@ -37,27 +31,29 @@ class BookShell extends Shell
     }
 
     /**
-     * main() method.
+     * Implement this method with your command's logic.
      *
-     * @return bool|int|null Success or error code.
+     * @param \Cake\Console\Arguments $args The command arguments.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     * @return null|int The exit code or null for success
      */
-    public function main()
+    public function execute(Arguments $args, ConsoleIo $io)
     {
-        $query = urlencode(implode(' ', $this->args));
+        $query = $args->getArgument('parameter');
         $version = $this->getVersion();
         $this->client = new Client();
         $results = json_decode((string)$this->client->get("https://search.cakephp.org/search?lang=en&q=$query&version={$version}")->getBody(), true);
         //$results = Cache::read('book.' . $query, 'book');
 
         if ($this->isFifoOutput()) {
-            $this->echoAllResults($results['data'] ?? []);
+            $this->echoAllResults($results['data'] ?? [], $io);
 
             return 0;
         }
 
         $count = count($results['data']);
         if ($count == 0) {
-            $this->out(__('  No results found!'));
+            $io->err(__('  No results found!'));
 
             return 1;
         }
@@ -65,19 +61,19 @@ class BookShell extends Shell
         $options = [];
         foreach ($results['data'] as $index => $result) {
             $options[$index] = $index + 1;
-            $this->success("[{$options[$index]}]", false);
-            $this->info(__(' {1}:', $options[$index], $result['title']), false);
-            $this->out(str_replace("\n", '. ', $result['contents'][0] ?? 'N/A'));
-            $this->out(__('   ' . $this->getUrl($result)));
+            $io->success("[{$options[$index]}]", false);
+            $io->info(__(' {1}:', $options[$index], $result['title']), false);
+            $io->out(str_replace("\n", '. ', $result['contents'][0] ?? 'N/A'));
+            $io->out(__('   ' . $this->getUrl($result)));
         }
 
         do {
-            $topic = $this->in(__('Please select the topic that you want to read [1-{0}]', $count));
+            $topic = $io->ask(__('Please select the topic that you want to read [1-{0}]', $count));
         } while (!is_numeric($topic) || (is_numeric($topic) && empty($results['data'][$topic - 1]['url'])));
 
         $result = $results['data'][$topic - 1];
-        $this->out($this->getContent($result));
-        $this->out($this->getUrl($result));
+        $io->out($this->getContent($result));
+        $io->out($this->getUrl($result));
     }
 
     protected function getContent($result): string
@@ -95,14 +91,14 @@ class BookShell extends Shell
         return substr(Configure::version(), 0, 1);
     }
 
-    protected function echoAllResults($results): void
+    protected function echoAllResults($results, ConsoleIo $io): void
     {
         $limit = 10;
         $i = 0;
         try {
             for ($i = 0; $i < count($results) && $i < $limit; $i++) {
-                $this->out(__('From page: ' . $this->getUrl($results[$i])));
-                $this->out($this->getContent($results[$i]));
+                $io->out(__('From page: ' . $this->getUrl($results[$i])));
+                $io->out($this->getContent($results[$i]));
             }
         } catch (\Exception $ex) {
             // ignore broken pipes
